d8bc5ce6aed8d5cf89d2a39a15871ffcc3bd0b8a
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-15 20:58:49 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,11 +11,11 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
   
 assembled_aoh = [
-  {:name => "Don Gately", :occupation => "Live-in Staff"},
-  {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
-  {:name => "Pat Monteseian", :occupation => "Staff"},
-  {:name => "Kate Gompert", :occupation => "None"},
-  {:name => "Bruce Green", :occupation => "Fan of Mildred"}
+  DON_G,
+  JOELLE_VD,
+  PAT_M,
+  KATE_G,
+  BRUCE_G
 ]
 
 end
